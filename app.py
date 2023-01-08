f2582aea14585a59db9249fe31a24e1c8550c8be
bhuwanonline <bhuwanonline@gmail.com>
2023-01-09 02:01:54 +0530

    Update app.py
    Modified labels

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home- CI CD</h3>"
+    html = "<h3>Sklearn Prediction Home- Building a CI/CD Pipeline</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
@@ -43,7 +43,7 @@ def predict():
     LOG.info("inference payload DataFrame: %s inference_payload")
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': prediction})
+    return jsonify({'output prediction': prediction})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
