bcdb9c27793a119b68dd4c839d68477c090852d9
bhuwanonline <bhuwanonline@gmail.com>
2023-01-10 00:15:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home- Building a CI/CD Pipeline</h3>"
+    html = "<h3>Sklearn Prediction Home- Building a CI/CD Pipeline Udacity</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
